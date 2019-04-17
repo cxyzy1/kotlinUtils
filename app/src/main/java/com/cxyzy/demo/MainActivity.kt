e318ab3799f351ba5df8bb0db100e9ace510b521
jerry <309032663@qq.com>
2019-04-17 13:51:01 +0800

    add toast

--- app/src/main/java/com/cxyzy/demo/MainActivity.kt
+++ app/src/main/java/com/cxyzy/demo/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.cxyzy.kt.utils
+package com.cxyzy.demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cxyzy.kt.utils.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toast("test")
     }
 }
