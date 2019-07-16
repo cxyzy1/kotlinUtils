9759b5a42ff30048853a64584182662fa08cd074
jerry <309032663@qq.com>
2019-07-16 10:20:29 +0800

    update

--- library/src/main/java/com/cxyzy/kt/utils/ToastExt.kt
+++ library/src/main/java/com/cxyzy/kt/utils/ToastExt.kt
@@ -1,17 +1,16 @@
 package com.cxyzy.kt.utils
 
 
-import android.app.Fragment
 import android.content.Context
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 /**
  * Display the simple Toast message with the [Toast.LENGTH_SHORT] duration.
  *
  * @param message the message text resource.
  */
-@Deprecated(message = "Use support library fragments instead. Framework fragments were deprecated in API 28.")
-inline fun Fragment.toast(message: Int) = activity.toast(message)
+inline fun Fragment.toast(message: Int) = activity?.toast(message)
 
 /**
  * Display the simple Toast message with the [Toast.LENGTH_SHORT] duration.
@@ -29,8 +28,7 @@ inline fun Context.toast(message: Int): Toast = Toast
  *
  * @param message the message text.
  */
-@Deprecated(message = "Use support library fragments instead. Framework fragments were deprecated in API 28.")
-inline fun Fragment.toast(message: CharSequence) = activity.toast(message)
+inline fun Fragment.toast(message: CharSequence) = activity?.toast(message)
 
 /**
  * Display the simple Toast message with the [Toast.LENGTH_SHORT] duration.
@@ -48,8 +46,7 @@ inline fun Context.toast(message: CharSequence): Toast = Toast
  *
  * @param message the message text resource.
  */
-@Deprecated(message = "Use support library fragments instead. Framework fragments were deprecated in API 28.")
-inline fun Fragment.longToast(message: Int) = activity.longToast(message)
+inline fun Fragment.longToast(message: Int) = activity?.longToast(message)
 
 /**
  * Display the simple Toast message with the [Toast.LENGTH_LONG] duration.
@@ -67,8 +64,7 @@ inline fun Context.longToast(message: Int): Toast = Toast
  *
  * @param message the message text.
  */
-@Deprecated(message = "Use support library fragments instead. Framework fragments were deprecated in API 28.")
-inline fun Fragment.longToast(message: CharSequence) = activity.longToast(message)
+inline fun Fragment.longToast(message: CharSequence) = activity?.longToast(message)
 
 /**
  * Display the simple Toast message with the [Toast.LENGTH_LONG] duration.
