337022b9e2886fcbe0506fca4c9e836f912f0633
jerry <309032663@qq.com>
2019-07-16 17:38:58 +0800

    update

---
+++ library/src/main/java/com/cxyzy/kt/utils/IntentExt.kt
@@ -0,0 +1,220 @@
+package com.cxyzy.kt.utils
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Service
+import android.content.ActivityNotFoundException
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Parcelable
+import java.io.Serializable
+
+/**
+ * Intent工具类
+ */
+inline fun <reified T : Activity> Context.startActivity(vararg params: Pair<String, Any?>) =
+    AnkoInternals.internalStartActivity(this, T::class.java, params)
+
+inline fun <reified T : Activity> Activity.startActivityForResult(requestCode: Int, vararg params: Pair<String, Any?>) =
+    AnkoInternals.internalStartActivityForResult(this, T::class.java, requestCode, params)
+
+inline fun <reified T : Service> Context.startService(vararg params: Pair<String, Any?>) =
+    AnkoInternals.internalStartService(this, T::class.java, params)
+
+inline fun <reified T : Service> Context.stopService(vararg params: Pair<String, Any?>) =
+    AnkoInternals.internalStopService(this, T::class.java, params)
+
+inline fun Intent.clearTask(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) }
+
+inline fun Intent.clearTop(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) }
+
+
+inline fun Intent.excludeFromRecents(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS) }
+
+/**
+ * Add the [Intent.FLAG_ACTIVITY_MULTIPLE_TASK] flag to the [Intent].
+ *
+ * @return the same intent with the flag applied.
+ */
+inline fun Intent.multipleTask(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK) }
+
+/**
+ * Add the [Intent.FLAG_ACTIVITY_NEW_TASK] flag to the [Intent].
+ *
+ * @return the same intent with the flag applied.
+ */
+inline fun Intent.newTask(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
+
+/**
+ * Add the [Intent.FLAG_ACTIVITY_NO_ANIMATION] flag to the [Intent].
+ *
+ * @return the same intent with the flag applied.
+ */
+inline fun Intent.noAnimation(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION) }
+
+/**
+ * Add the [Intent.FLAG_ACTIVITY_NO_HISTORY] flag to the [Intent].
+ *
+ * @return the same intent with the flag applied.
+ */
+inline fun Intent.noHistory(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY) }
+
+/**
+ * Add the [Intent.FLAG_ACTIVITY_SINGLE_TOP] flag to the [Intent].
+ *
+ * @return the same intent with the flag applied.
+ */
+inline fun Intent.singleTop(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP) }
+
+fun Context.browse(url: String, newTask: Boolean = false): Boolean {
+    return try {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        if (newTask) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+        true
+    } catch (e: ActivityNotFoundException) {
+        e.printStackTrace()
+        false
+    }
+}
+
+fun Context.share(text: String, subject: String = ""): Boolean {
+    return try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        startActivity(Intent.createChooser(intent, null))
+        true
+    } catch (e: ActivityNotFoundException) {
+        e.printStackTrace()
+        false
+    }
+}
+
+fun Context.email(email: String, subject: String = "", text: String = ""): Boolean {
+    val intent = Intent(Intent.ACTION_SENDTO)
+    intent.data = Uri.parse("mailto:")
+    intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+    if (subject.isNotEmpty())
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+    if (text.isNotEmpty())
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+    if (intent.resolveActivity(packageManager) != null) {
+        startActivity(intent)
+        return true
+    }
+    return false
+
+}
+
+@SuppressLint("MissingPermission")
+fun Context.makeCall(number: String): Boolean {
+    return try {
+        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:$number"))
+        startActivity(intent)
+        true
+    } catch (e: Exception) {
+        e.printStackTrace()
+        false
+    }
+}
+
+fun Context.sendSMS(number: String, text: String = ""): Boolean {
+    return try {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("sms:$number"))
+        intent.putExtra("sms_body", text)
+        startActivity(intent)
+        true
+    } catch (e: Exception) {
+        e.printStackTrace()
+        false
+    }
+}
+
+object AnkoInternals {
+
+    @JvmStatic
+    fun <T> createIntent(ctx: Context, clazz: Class<out T>, params: Array<out Pair<String, Any?>>): Intent {
+        val intent = Intent(ctx, clazz)
+        if (params.isNotEmpty()) fillIntentArguments(intent, params)
+        return intent
+    }
+
+    @JvmStatic
+    fun internalStartActivity(
+        ctx: Context,
+        activity: Class<out Activity>,
+        params: Array<out Pair<String, Any?>>
+    ) {
+        ctx.startActivity(createIntent(ctx, activity, params))
+    }
+
+    @JvmStatic
+    fun internalStartActivityForResult(
+        act: Activity,
+        activity: Class<out Activity>,
+        requestCode: Int,
+        params: Array<out Pair<String, Any?>>
+    ) {
+        act.startActivityForResult(createIntent(act, activity, params), requestCode)
+    }
+
+    @JvmStatic
+    fun internalStartService(
+        ctx: Context,
+        service: Class<out Service>,
+        params: Array<out Pair<String, Any?>>
+    ): ComponentName? = ctx.startService(createIntent(ctx, service, params))
+
+    @JvmStatic
+    fun internalStopService(
+        ctx: Context,
+        service: Class<out Service>,
+        params: Array<out Pair<String, Any?>>
+    ): Boolean = ctx.stopService(createIntent(ctx, service, params))
+
+    @JvmStatic
+    private fun fillIntentArguments(intent: Intent, params: Array<out Pair<String, Any?>>) {
+        params.forEach {
+            val value = it.second
+            when (value) {
+                null -> intent.putExtra(it.first, null as Serializable?)
+                is Int -> intent.putExtra(it.first, value)
+                is Long -> intent.putExtra(it.first, value)
+                is CharSequence -> intent.putExtra(it.first, value)
+                is String -> intent.putExtra(it.first, value)
+                is Float -> intent.putExtra(it.first, value)
+                is Double -> intent.putExtra(it.first, value)
+                is Char -> intent.putExtra(it.first, value)
+                is Short -> intent.putExtra(it.first, value)
+                is Boolean -> intent.putExtra(it.first, value)
+                is Serializable -> intent.putExtra(it.first, value)
+                is Bundle -> intent.putExtra(it.first, value)
+                is Parcelable -> intent.putExtra(it.first, value)
+                is Array<*> -> when {
+                    value.isArrayOf<CharSequence>() -> intent.putExtra(it.first, value)
+                    value.isArrayOf<String>() -> intent.putExtra(it.first, value)
+                    value.isArrayOf<Parcelable>() -> intent.putExtra(it.first, value)
+                    else -> throw Exception("Intent extra ${it.first} has wrong type ${value.javaClass.name}")
+                }
+                is IntArray -> intent.putExtra(it.first, value)
+                is LongArray -> intent.putExtra(it.first, value)
+                is FloatArray -> intent.putExtra(it.first, value)
+                is DoubleArray -> intent.putExtra(it.first, value)
+                is CharArray -> intent.putExtra(it.first, value)
+                is ShortArray -> intent.putExtra(it.first, value)
+                is BooleanArray -> intent.putExtra(it.first, value)
+                else -> throw Exception("Intent extra ${it.first} has wrong type ${value.javaClass.name}")
+            }
+            return@forEach
+        }
+    }
+
+}
