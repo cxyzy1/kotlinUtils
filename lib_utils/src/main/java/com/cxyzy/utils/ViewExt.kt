63da170a92accfe15ec26f828124f0a73d8a6c20
jerry <309032663@qq.com>
2020-04-16 14:35:48 +0800

    update

---
+++ lib_utils/src/main/java/com/cxyzy/utils/ViewExt.kt
@@ -0,0 +1,11 @@
+package com.cxyzy.utils
+
+import android.view.View
+
+fun View.show(isVisible: Boolean) {
+    this.visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+fun View.show() = show(true)
+
+fun View.hide() = show(false)
